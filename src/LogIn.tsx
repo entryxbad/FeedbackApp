b2912dc00681a7f941e333f472afb429eff79c3b
Радмир Карманов <karr@soft-servis.local>
2023-03-03 12:08:24 +0500

    add input number mask

--- src/LogIn.tsx
+++ src/LogIn.tsx
@@ -6,13 +6,52 @@ import {
   TouchableOpacity,
 } from 'react-native';
 import React from 'react';
+import MaskInput from 'react-native-mask-input';
 
 const LogIn = ({navigation}) => {
+  const [phone, setPhone] = React.useState('');
+
   return (
     <View style={styles.wrapper}>
       <Text style={styles.headerText}>Войдите в учётную запись</Text>
-      <TextInput style={styles.input} placeholder="Логин"></TextInput>
-      <TextInput style={styles.input} placeholder="Пароль"></TextInput>
+      <MaskInput
+        style={styles.input}
+        value={phone}
+        placeholder="Логин"
+        keyboardType="numeric"
+        onChangeText={(masked, unmasked) => {
+          setPhone(masked); // you can use the unmasked value as well
+
+          // assuming you typed "9" all the way:
+          console.log(masked); // (99) 99999-9999
+          console.log(unmasked); // 99999999999
+        }}
+        mask={[
+          '+',
+          '7',
+          ' ',
+          '(',
+          /\d/,
+          /\d/,
+          /\d/,
+          ')',
+          ' ',
+          /\d/,
+          /\d/,
+          /\d/,
+          '-',
+          /\d/,
+          /\d/,
+          '-',
+          /\d/,
+          /\d/,
+        ]}
+      />
+
+      <TextInput
+        style={styles.input}
+        secureTextEntry={true}
+        placeholder="Пароль"></TextInput>
       <TouchableOpacity
         style={styles.button}
         onPress={() => navigation.navigate('Home')}>
