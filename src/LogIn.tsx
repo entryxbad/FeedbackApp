51a9ee9f7626ed2103049c6a1904160736d792e8
Радмир Карманов <karr@soft-servis.local>
2023-03-03 12:13:06 +0500

    delete clg

--- src/LogIn.tsx
+++ src/LogIn.tsx
@@ -5,11 +5,11 @@ import {
   TextInput,
   TouchableOpacity,
 } from 'react-native';
-import React from 'react';
+import React, {useState} from 'react';
 import MaskInput from 'react-native-mask-input';
 
 const LogIn = ({navigation}) => {
-  const [phone, setPhone] = React.useState('');
+  const [phone, setPhone] = useState('');
 
   return (
     <View style={styles.wrapper}>
@@ -19,12 +19,8 @@ const LogIn = ({navigation}) => {
         value={phone}
         placeholder="Логин"
         keyboardType="numeric"
-        onChangeText={(masked, unmasked) => {
-          setPhone(masked); // you can use the unmasked value as well
-
-          // assuming you typed "9" all the way:
-          console.log(masked); // (99) 99999-9999
-          console.log(unmasked); // 99999999999
+        onChangeText={(masked) => {
+          setPhone(masked);
         }}
         mask={[
           '+',
