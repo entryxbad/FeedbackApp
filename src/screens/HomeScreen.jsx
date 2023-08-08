fa357b1a47a3271582f5a4f9819ff7925683325a
Radmir <radmir_96@mail.ru>
2023-08-08 15:47:22 +0500

    add logo from server

--- src/screens/HomeScreen.jsx
+++ src/screens/HomeScreen.jsx
@@ -20,43 +20,47 @@ export const HomeScreen = ({ navigation }) => {
 
   return (
     <View
-      className='flex-1 items-center justify-center'
+      className='flex-1 relative'
       style={{ backgroundColor: robotData?.backgroundColor }}
     >
-      {/* Logo //FIXME*/}
-      <View className='flex-1 absolute items-center justify-center'>
+      {/* Logo */}
+      <View className='flex-row relative p-4 justify-center'>
         <Image
-          className='w-full h-full object-cover'
+          className='w-96 h-80 object-cover rounded-2xl'
           source={{
             uri: `https://robominds.soft-servis.ru/${robotData?.logo}`
           }}
         />
       </View>
-      {/* Button start */}
-      <AnimatedButton
-        onPress={() => navigation.navigate('Questions')}
-        buttonStyle={
-          'absolute items-center rounded-2xl border-l-2 border-r-2 border-t-4 w-[60%] py-1'
-        }
-        textStyle={'text-4xl py-4'}
-        text={'Начать опрос'}
-        style={{
-          borderColor: robotData?.buttonColor || '#1a75d4',
-          color: robotData?.fontColor || '#000'
-        }}
-      />
-      {/* Button exit */}
-      <Button
-        onPress={() => navigation.navigate('Logout')}
-        text={'Выйти из аккаунта'}
-        buttonStyle={'top-56'}
-        textStyle={'text-4xl py-4 px-4'}
-        style={{
-          color: robotData?.fontColor || '#000',
-          backgroundColor: robotData?.buttonColor || '#1a75d4',
-          borderRadius: 20
-        }}
-      />
+
+      <View className='flex-col items-center justify-center mt-5'>
+        {/* Button start */}
+        <AnimatedButton
+          onPress={() => navigation.navigate('Questions')}
+          buttonStyle={
+            'absolute items-center rounded-2xl border-l-2 border-r-2 border-t-4 w-[60%] py-1'
+          }
+          textStyle={'text-4xl py-4'}
+          text={'Начать опрос'}
+          style={{
+            borderColor: robotData?.buttonColor || '#1a75d4',
+            color: robotData?.fontColor || '#000'
+          }}
+        />
+
+        {/* Button exit */}
+        <Button
+          onPress={() => navigation.navigate('Logout')}
+          text={'Выйти из аккаунта'}
+          buttonStyle={'top-56'}
+          textStyle={'text-4xl py-4 px-4'}
+          style={{
+            color: robotData?.fontColor || '#000',
+            backgroundColor: robotData?.buttonColor || '#1a75d4',
+            borderRadius: 20
+          }}
+        />
+      </View>
     </View>
   )
 }
